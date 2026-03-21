2ace34c73f205c4b40b6c4aa24bfa6f2e3eee8eb
НОУТ <sem@mail.com>
2026-03-21 07:55:06 +0500

    исправил стратегию

--- utils.py
+++ utils.py
@@ -306,11 +306,11 @@ def filter_list(interval, figi: str, tiker: str, tuple_indicator: tuple):
             """ДНЕВНОЙ ИНТЕРВАЛ УСЛОВИЕ"""
             timeframe = "day"
             # ПОКУПКА
-            # 1_buy_day) возраст SMA 10.RSI<65 MACD _-'   МАКСИМАЛЬНО УЖЕСТОЧИТЬ
-            # (last_sma_10_3 < last_sma_10_2 < last_sma_10_1) and (prev_rsi < last_rsi < 70) and (prev_MACD<last_MACD)
+            # 1_buy_day) возраст SMA 10.RSI<75 MACD _-'   МАКСИМАЛЬНО УЖЕСТОЧИТЬ
+            # (last_sma_10_3 < last_sma_10_2 < last_sma_10_1) and (prev_rsi < last_rsi < 75) and (prev_MACD<last_MACD)
             if (
                 (tuple_indicator[10] < tuple_indicator[9] < tuple_indicator[8])
-                and (tuple_indicator[1] < tuple_indicator[0] < 70)
+                and (tuple_indicator[1] < tuple_indicator[0] < 75)
                 and (tuple_indicator[5] < tuple_indicator[4])
             ):
                 """ВОЗРОСТАНИЕ SMA 10 , RSI, MACD"""
@@ -319,7 +319,7 @@ def filter_list(interval, figi: str, tiker: str, tuple_indicator: tuple):
                     tiker=tiker,
                     timeframe=timeframe,
                     action="buy",
-                    strategy=f"1_buy_{timeframe}) возраст SMA 10.RSI<70 MACD _-'",
+                    strategy=f"1_buy_{timeframe}) возраст SMA 10.RSI<75 MACD _-'",
                     description=f"last_MACD:{round(tuple_indicator[4], 2)}, last_rsi:{round(tuple_indicator[0], 2)}, midBoll:{round(tuple_indicator[13], 2)}",
                 )
                 session.add(filter_tiker)
@@ -351,11 +351,11 @@ def filter_list(interval, figi: str, tiker: str, tuple_indicator: tuple):
             #                                     description=f"last_MACD:{round(tuple_indicator[4], 2)}, last_rsi:{round(tuple_indicator[0], 2)}, midBoll:{round(tuple_indicator[13], 2)}")
             #     session.add(filter_tiker)
             #     session.commit()
-            # 1_sell_day) убывающий SMA 10.RSI>30 '-_   МАКСИМАЛЬНО УЖЕСТОЧИТЬ
-            # (last_sma_10_1  < last_sma_10_2 < last_sma_10_3) and (30<last_rsi<prev_rsi) and (last_MACD<prev_MACD)
+            # 1_sell_day) убывающий SMA 10.RSI>25 '-_   МАКСИМАЛЬНО УЖЕСТОЧИТЬ
+            # (last_sma_10_1  < last_sma_10_2 < last_sma_10_3) and (25<last_rsi<prev_rsi) and (last_MACD<prev_MACD)
             elif (
                 (tuple_indicator[8] < tuple_indicator[9] < tuple_indicator[10])
-                and (30 < tuple_indicator[0] < tuple_indicator[1])
+                and (25 < tuple_indicator[0] < tuple_indicator[1])
                 and (tuple_indicator[4] < tuple_indicator[5])
             ):
                 """УБЫВАЮЩИЙ SMA 10 , RSI, MACD '-_"""
@@ -364,7 +364,7 @@ def filter_list(interval, figi: str, tiker: str, tuple_indicator: tuple):
                     tiker=tiker,
                     timeframe=timeframe,
                     action="sell",
-                    strategy=f"1_sell_{timeframe}) убывающий SMA 10.RSI>30 MACD'-_ ",
+                    strategy=f"1_sell_{timeframe}) убывающий SMA 10.RSI>25 MACD'-_ ",
                     description=f"last_MACD:{round(tuple_indicator[4], 2)}, last_rsi:{round(tuple_indicator[0], 2)}, midBoll:{round(tuple_indicator[13], 2)}",
                 )
                 session.add(filter_tiker)
@@ -377,8 +377,8 @@ def filter_list(interval, figi: str, tiker: str, tuple_indicator: tuple):
             timeframe = "hour"
             # ПОКУПКА
             # 1_buy_hour) возраст SMA10
-            # last_sma_10_2<last_sma_10_1)
-            if tuple_indicator[9] < tuple_indicator[8]:
+            # last_sma_10_3<last_sma_10_2<last_sma_10_1)
+            if tuple_indicator[10] < tuple_indicator[9] < tuple_indicator[8]:
                 """ВОЗРОСТАНИЕ SMA 10 """
                 buy_hour[tiker] = figi
                 filter_tiker = FilterTickerDict(
@@ -392,8 +392,8 @@ def filter_list(interval, figi: str, tiker: str, tuple_indicator: tuple):
                 session.commit()
             # ПРОДАЖА
             # 1_sell_hour) убывающий SMA10
-            # (last_sma_10_1<last_sma_10_2
-            elif tuple_indicator[8] < tuple_indicator[9]:
+            # (last_sma_10_1<last_sma_10_2<last_sma_10_3
+            elif tuple_indicator[8] < tuple_indicator[9] < tuple_indicator[10]:
                 """УБЫВАЮЩИЙ SMA 10 """
                 sale_hour[tiker] = figi
                 filter_tiker = FilterTickerDict(
@@ -1312,28 +1312,6 @@ def trading_information(cl, days: int, tm=2):
                         # logger.info(f"{tiker} - ЗАПИСЬ СУЩЕСТВУЕТ: Exception as e : {e}")
     except Exception as e:
         logger.info(f"trading_information() ошибка сбор информации о покупке и продаже: Ex as e : {e}")
-
-
-# -------НАЧАЛО ОТПРАВКА В ТЕЛЕГРАММ--------
-# def send_telegram(
-#     tupl: tuple,
-#     telegram_cl=TelegramClient(name="SEM", api_id=api_iddd, api_hash=telegtok, parse_mode=ParseMode.HTML),
-#     group=groupt,
-# ):
-#     """ОТПРАВЛЯЕТ В ТЕЛЕГРАММ НА ПОКУПКУ И ПРОДАЖУ"""
-#     try:
-#         with telegram_cl:
-#             telegram_cl.send_message(group, f"-----НАЧАЛО : {datetime.now().strftime('%d.%m.%Y %H:%M')}---")
-#             if tupl[0]:
-#                 telegram_cl.send_message(group, f"✅ ПОКУПКА : <b>{tupl[0].keys()}</b>")
-#             time.sleep(1)
-#             if tupl[1]:
-#                 telegram_cl.send_message(group, f"🔴 ПРОДАЖА : <b>{tupl[1].keys()}</b>")
-#             end_time_str = (datetime.now() + timedelta(seconds=530)).strftime("%d.%m.%Y %H:%M")
-#             telegram_cl.send_message(group, f"-----СЛЕДУЮЩИЙ : {end_time_str}-----")
-#             telegram_cl.send_message(group, "🧠")
-#     except Exception as e:
-#         logger.info(f"send_telegram() ошибка в телеграмм: Ex as e : {e}")
 
 
 def get_proxy_dict(proxy_url: str | None):
