2580f3bc4eba2b65919f86a4cafffd8547f1accc
Работа РТРС <sem87sem611sem@gmail.com>
2026-08-17 10:27:31 +0500

    разбираем с функцией сравнения тикеров

--- actualnost_ticker/actualnost.py
+++ actualnost_ticker/actualnost.py
@@ -1,10 +1,11 @@
-#ПРОВЕРЯЕМ АКТУАЛЬНЫ ЛИ ТИКЕРЫ
+# ПРОВЕРЯЕМ АКТУАЛЬНЫ ЛИ ТИКЕРЫ
 from log.logger import inform, logger
 import json
 import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from typing import Any
+import pandas as pd
 from t_tech.invest import (
     CandleInterval,
     InstrumentIdType,
@@ -14,7 +15,7 @@ from t_tech.invest import (
     OrderDirection,
     OrderType,
     RequestError,
-    Client, # Не забудь импортировать Client
+    Client,  # Не забудь импортировать Client
 )
 from t_tech.invest.services import (
     InstrumentsService,
@@ -22,7 +23,7 @@ from t_tech.invest.services import (
     StopOrderExpirationType,
     StopOrderType,
 )
-
+from sql_base.sql_terminator_cuber import AnalysisTiker, session
 
 """ДЛЯ ЧТЕНИЯ ТОКЕНА"""
 load_dotenv("../terminator/.env.term")  # Если файл в той же папке, что и скрипт
@@ -34,27 +35,22 @@ api_iddd = os.getenv("API_IDDD")
 proxy_url = os.getenv("PROXY_URL")
 
 
-
-
-
 class ActualniiTiker:
     """Работа с актуальными тикерами и их FIGI."""
 
-    def __init__(self, token: str, file_path: str = "tiker_figi.json",days: int = 7) -> None:
+    def __init__(self, token: str, file_path: str = "tiker_figi.json", days: int = 7) -> None:
         self.token = token
-        self.file_path = file_path    #  это можно не указывать по умолчанию
+        self.file_path = file_path  # это можно не указывать по умолчанию
         self.days = days
         self._client = None  # Клиент создаётся по требованию    зачем это делать
+        self.tiker_figi = {}
 
     @property
     def client(self):
-        """Ленивое создание клиента."""
+        """ЛЕНИВОЕ СОЗДАНИЕ КЛИЕНТА"""
         if self._client is None:
-            print("подключаю клиента")
             self._client = Client(self.token)
-        print("клиент уже есть")
         return self._client
-
 
     def last_modified_json(self):
         """ПРОВЕРЯЕТ ДАВНО ЛИ ОБНОВЛЯЛСЯ tiker_figi.json"""
@@ -66,13 +62,46 @@ class ActualniiTiker:
                 delta = datetime.now() - datetime.fromtimestamp(last_modified)
                 if delta >= timedelta(days=self.days):
                     inform.info(f"Обновляем ФИГИ в tiker_figi.json. Прошло {delta.days} дней!!!")
-                    # save_all_json(cl=cl)
+                    self.save_all_json()
                     return True
                 else:
-                    inform.info(f"Файл был изменён менее 7 дней. Прошло только {delta.days} дней!!! ВСЕ ОК.")
+                    inform.info(f"Файл был изменён менее {self.days} дней. Прошло только {delta.days} дней!!! ВСЕ ОК.")
                     return False
         except Exception as e:
             logger.info(f"last_modified_json() - (ошибка) нет файла tiker_figi.json: Exception as e : {e}")
+
+    # ---------НАЧАЛО ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ----------------
+    def save_all_json(self):
+        """СОХРАНЯЕТ В JSON "tiker":"figi" """
+        try:
+            tikers = self.list_active_tickers()
+            for tiker in tikers:
+                self.tiker_figi[tiker] = self.get_figi(tiker=tiker, cl=self.client)
+            with open(self.file_path, "w", encoding="utf-8") as f:
+                json.dump(self.tiker_figi,f, indent=4, ensure_ascii=False, sort_keys=True)
+            return None
+        except Exception as e:
+            logger.info(f" - функция save_all_json - не получается сохранить JSON.Exception as e : {e}")
+            return None
+
+
+    def get_figi(self, cl, tiker: str):
+        """ИЗВЛЕКАЕТ ИЗ ТИКЕРА ФИГИ"""
+        try:
+            instruments: InstrumentsService = cl.instruments
+            # market_data: MarketDataService = cl.market_data
+            # Забирает данные для INSTRUMENT_STATUS_BASE
+            df = pd.DataFrame(
+                instruments.shares(instrument_status=InstrumentStatus.INSTRUMENT_STATUS_BASE).instruments,
+                columns=["name", "figi", "ticker", "class_code"],
+            )
+            # for method in ['shares' АКЦИИ, 'bonds', 'etfs' ]:
+            # 'currencies', 'futures']:  ОБЛИГАЦИИ ЕТФ ОПЦИОНЫ НУЖНО РАЗБИРАТЬСЯ
+            figi = df[df["ticker"] == tiker]["figi"].iloc[0]
+            return figi
+        except Exception as e:
+            logger.info(f" {tiker} - функция get_figi() - не получается достать figi Exception as e : {e}")
+            return None
 
 
     def read_tiker_figi_json(self) -> dict[str, Any]:
@@ -85,46 +114,35 @@ class ActualniiTiker:
         except OSError as e:
             logger.info(f"Ошибка чтения: {e}")
         return {}
+
     # # =================   нужно разобраться почему это все так   =================
-    # def save_all_json(self):
-    #     """СОХРАНЯЕТ В JSON "tiker":"figi" """
-    #     try:
-    #         tikers = list_active_tickers()
-    #         for tiker in tikers:
-    #             tiker_figi[tiker] = get_figi(tiker=tiker, cl=cl)
-    #         with open("tiker_figi.json", "w", encoding="utf-8") as f:
-    #             json.dump(tiker_figi, f, indent=4, ensure_ascii=False, sort_keys=True)
-    #         return None
-    #     except Exception as e:
-    #         logger.info(f" - функция save_all_json - не получается сохранить JSON.Exception as e : {e}")
-    #         return None
-    #
-    # def list_active_tickers():
-    #     """ПОЛУЧАЕМ СПИСОК ВСЕ АКЦИИ 'на рынке' ИЗ БАЗЫ"""
-    #     try:
-    #         active_tickers = session.query(AnalysisTiker.tiker).filter(AnalysisTiker.activity == "на рынке").all()
-    #         active_tickers = [row[0] for row in active_tickers]
-    #         # print(f"СПИСОК АКТИВНЫХ АКЦИЙ {active_tickers}")
-    #         return active_tickers
-    #     except Exception as e:
-    #         logger.info(
-    #             f"list_active_tickers() - не получается достать ВСЕ АКЦИИ 'на рынке' ИЗ БАЗЫ Exception as e : {e}")
-    # # =================   нужно разобраться почему это все так   =================
-    # #  что еще реализовывается в классе что можно сделать
+
+    def list_active_tickers(self):
+        """ПОЛУЧАЕМ СПИСОК ВСЕ АКЦИИ 'на рынке' ИЗ БАЗЫ"""
+        try:
+            active_tickers = session.query(AnalysisTiker.tiker).filter(AnalysisTiker.activity == "на рынке").all()
+            active_tickers = [row[0] for row in active_tickers]
+            print(f"СПИСОК АКТИВНЫХ АКЦИЙ {active_tickers}")
+            return active_tickers
+        except Exception as e:
+            logger.info(
+                f"list_active_tickers() - не получается достать ВСЕ АКЦИИ 'на рынке' ИЗ БАЗЫ Exception as e : {e}")
+
+    # =================   нужно разобраться почему это все так   =================
+    #  что еще реализовывается в классе что можно сделать
+
+    # ---------КОНЕЦ ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ----------------
 
     def __enter__(self):
-        print("ентер")
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print("ексит")
         if self._client is not None:
             self._client.close()  # ← закрываем при выходе из with
             self._client = None
 
-
-
-
+    def __str__(self):
+        return f"Срабатывает класс с количеством дней {self.days}"
 
 
 if __name__ == "__main__":
